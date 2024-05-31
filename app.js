f959d3e431bb0d1a138bca840df661773085d994
Tin Zar Paing <tinzarpaing83@gmail.com>
2024-05-30 21:44:09 -0700

    feat: create basic structure for backend

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const bodyParser = require('body-parser');
+//const router = require('./routes'); //same as ('./routes/index')
+
+require('./models/db');
+
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+//app.use('/api/v1', router)
+
+app.get("/", (req, res, next) => {
+   res.json({
+        status:200,
+        message: "Hello from Getchup!!"
+   })
+});
+
+app.get("/*", (req, res, next) => {
+    res.contentType = 'text/plain';
+    res.status(404);
+    res.send("Not found");
+});
+
+app.listen(8080, () => {
+    console.log("Server running on port 8080.");
+});
