717ab723cb398f92dd79f46a8d5cfd2fc72275d7
greeshma017 <gdharmapalan00@mylangara.ca>
2024-06-13 14:12:56 -0700

    feat: BC-124 add common task controller, model, routes

--- routes/commonTask.js
+++ routes/commonTask.js
@@ -4,10 +4,7 @@ const router = express.Router({mergeParams:true});
 const commonTaskCtrl= require('../controllers/commonTaskController');
 
 router.post("/commonTasks", commonTaskCtrl.saveCommonTask);
-<<<<<<< HEAD
 router.get("/commonTasks", commonTaskCtrl.getCommonTask);
 router.delete('/commonTasks/:id', commonTaskCtrl.deleteCommonTask);
-=======
->>>>>>> f8d318b (feat: BC-124 add common task controller, model, routes)
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router({mergeParams:true});
 
 const taskRouter = require('./task');
+const commonTaskRouter = require('./commonTask');
 const authRouter = require('./auth');
 const commonTaskRouter = require('./commonTask');
 
 router.use(taskRouter);
+router.use(commonTaskRouter);
 router.use(authRouter);
 
 router.use(commonTaskRouter);
