605209beb943865b75f83a8e6f03d07546b994c1
greeshma017 <gdharmapalan00@mylangara.ca>
2024-06-13 16:36:20 -0700

    feat: BC-124 add endpoint for getCommonTask and delete

--- routes/commonTask.js
+++ routes/commonTask.js
@@ -4,5 +4,7 @@ const router = express.Router({mergeParams:true});
 const commonTaskCtrl= require('../controllers/commonTaskController');
 
 router.post("/commonTasks", commonTaskCtrl.saveCommonTask);
+router.get("/commonTasks", commonTaskCtrl.getCommonTask);
+router.delete('/commonTasks/:id', commonTaskCtrl.deleteCommonTask);
 
 module.exports = router;
