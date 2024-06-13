7b282e2d57615f78c82f2e5f705abc874bd598c5
harleen251 <h251@mylangara.ca>
2024-06-13 16:50:07 -0700

    add endpoint for login and create account

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { login } = require('../controllers/authController');
+const { login, createAccount } = require('../controllers/authController');
 const router = express.Router();
 
 router.post('/login', login);
+router.post('/createAccount', createAccount)
 
 module.exports = router;
