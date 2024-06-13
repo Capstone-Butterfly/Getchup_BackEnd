493a9749d2cbdab4b2a77e7753f7c09a1f510734
harleen251 <h251@mylangara.ca>
2024-06-13 14:01:09 -0700

    fixed the index file

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router({mergeParams:true});
 
 const taskRouter = require('./task');
+const authRouter = require('./auth');
 const commonTaskRouter = require('./commonTask');
 
 router.use(taskRouter);
+router.use(authRouter);
+
 router.use(commonTaskRouter);
 
 module.exports = router;
