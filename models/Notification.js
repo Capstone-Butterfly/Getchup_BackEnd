92a846345a8fb27110c33ec47f190611f24f90ba
Maria Beatriz Camargo <mbeatrizcamargo@hotmail.com>
2024-07-05 02:23:28 -0700

    feat: notifications backed created

--- models/Notification.js
+++ models/Notification.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema;
 
 const NotificationSchema = new Schema({
     identifier: { type: String, required: true },
-    task_id: { type: Schema.Types.ObjectId, ref: 'Task', required: true },
+    user_id: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    task_id: { type: Schema.Types.ObjectId, required: true },
     message: { type: String, required: true },
     sent_at: { type: Date, required: true },
     read: {type: Boolean, required: true, default: false}
--- routes/notification.js
+++ routes/notification.js
@@ -4,6 +4,7 @@ const router = express.Router({mergeParams:true});
 const notificationCtrl = require('../controllers/notificationController');
 
 // Route to save a notification
-router.post('/notifications', notificationCtrl.saveNotification);
+router.post("/notifications", notificationCtrl.saveNotification);
+router.get("/notifications/user/:userId", notificationCtrl.getNotificationsByUser);
 
 module.exports = router;
