68ac4960b1774a3b4b0ab95ba0f76258ad951e9d
Maria Beatriz Camargo <mbeatrizcamargo@hotmail.com>
2024-07-07 16:51:45 -0700

    feat: added patch notification function

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -5,12 +5,12 @@ const saveNotification = async (req, res) => {
         const notification = new Notification(req.body);
         const result = await notification.save();
         const urlStr = `/api/v1/notifications/${result.id}`;
-    console.log("saving")
-    // Set content-location header
-    res.set("content-location", urlStr);
-    res.status(201).json({ url: urlStr, data: result });
-  } catch (error) {
-      console.log("not saving")
+        console.log("saving")
+        // Set content-location header
+        res.set("content-location", urlStr);
+        res.status(201).json({ url: urlStr, data: result });
+    } catch (error) {
+        console.log("not saving")
         res.status(500).json({ message: 'Error saving notification' });
     }
 };
@@ -18,15 +18,37 @@ const saveNotification = async (req, res) => {
 const getNotificationsByUser = async (req, res) => {
     const userId = req.params.userId;
     try {
-      const notifications = await Notification.find({ user_id: userId }).exec();
-      if (notifications.length === 0) {
-        res.status(404).json({ message: "No notifications found for this user." });
-      } else {
-        res.status(200).json(notifications);
-      }
+        const notifications = await Notification.find({ user_id: userId }).exec();
+        if (notifications.length === 0) {
+            res.status(404).json({ message: "No notifications found for this user." });
+        } else {
+            res.status(200).json(notifications);
+        }
     } catch (error) {
-      res.status(500).json(error);
+        res.status(500).json(error);
     }
-  };
+};
 
-module.exports = { saveNotification, getNotificationsByUser };
+const markNotificationAsRead = async (req, res) => {
+    try {
+        const notificationId = req.params.notificationId; // Corrected this line
+
+        const notification = await Notification.findById(notificationId);
+
+        if (!notification) {
+            return res.status(404).json({ error: 'Notification not found' });
+        }
+        notification.read = true;
+
+        const updatedNotification = await notification.save();
+
+        res.status(200).json({
+            message: 'Notification updated',
+            data: updatedNotification,
+        });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+module.exports = { saveNotification, getNotificationsByUser, markNotificationAsRead };
